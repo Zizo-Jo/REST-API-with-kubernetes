b6270184764cb07cd95aef0f490d4ed53f0b4654
mininet <zihao.zhou01@estudiant.upf.edu>
2025-05-29 19:31:15 +0200

    added codes to show pod ip

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
-import os
+from flask import Flask, render_template, jsonify, redirect, url_for
+import socket
+import time
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
+    # Get pod IP address
+    pod_ip = socket.gethostbyname(socket.gethostname())
+    
+    # Show IP address page for 1 second before redirecting
+    return render_template('ip_display.html', pod_ip=pod_ip)
+
+@app.route("/main")
+def main():
     return render_template("index.html")
 
 if __name__ == '__main__':
